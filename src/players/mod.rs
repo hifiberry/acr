065cdf00e8ae8bdf012d1111067a907cdbdeb51d
HiFiBerry <info@hifiberry.com>
2025-04-29 17:07:09 +0200

    started RAAT implementation

--- src/players/mod.rs
+++ src/players/mod.rs
@@ -4,6 +4,7 @@ mod base_controller;
 mod mpd;
 mod null_controller;
 pub mod player_factory;
+mod raat;
 
 // Re-export the PlayerController trait and related components
 pub use player_controller::{PlayerController, PlayerStateListener};
@@ -11,4 +12,5 @@ pub use base_controller::BasePlayerController;
 pub use mpd::MPDPlayerController;
 pub use null_controller::NullPlayerController;
 pub use player_factory::{create_player_from_json, create_player_from_json_str, PlayerCreationError, sample_json_config};
+pub use raat::MetadataPipeReader;
 

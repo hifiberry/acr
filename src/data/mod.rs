908f8c8ceb4876d037d40e58e81fbcb135bf942d
HiFiBerry <info@hifiberry.com>
2025-04-29 20:36:54 +0200

    add enumflags

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -5,9 +5,10 @@ pub mod loop_mode;
 pub mod player;
 pub mod player_command;
 pub mod player_event;
+pub mod player_state;
 pub mod serializable;
 pub mod song;
-pub mod stream_details;
+pub mod stream_details; // Add the new module
 
 // Re-export types from child modules
 pub use capabilities::*;
@@ -15,6 +16,7 @@ pub use loop_mode::*;
 pub use player::*;
 pub use player_command::*;
 pub use player_event::*;
+pub use player_state::*;
 pub use serializable::*;
 pub use song::*;
-pub use stream_details::*;
+pub use stream_details::*; // Re-export the new module
